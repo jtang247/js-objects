3de8cde6ef0ebb4bb4fe87850450bf279a4372a3
jkt247 <Jonathon@sos-hawaii.com>
2017-01-13 20:15:21 -1000

    objects

--- exercises.js
+++ exercises.js
@@ -6,6 +6,12 @@
             - Add a property named `size` and set it to a number value between `0` and `20`.
             - Add a property named `contents` and set it to be an empty array.
  */
+var plainBox = {
+    color: "Blue",
+    size: "4",
+    contents: []
+}
+console.log(plainBox);
 
 
 /*
@@ -22,7 +28,13 @@
             - add a key named `driver` and set it to be `null`.
             - add a key named `passengers` and set it to be an empty array.
  */
-
+var stockCar={};
+stockCar.model="Audi";
+stockCar.year="2006";
+stockCar.automaticTransmission=false;
+stockCar.driver=null;
+stockCar.passengers=[];
+console.log(stockCar);
 
 /*
     Add new property inside a function
@@ -37,7 +49,17 @@
 
         Finally, invoke your new function while passing in the `plainPerson` object and additional agrument values, then store the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just the value at `name`, and just the value at `age`.
  */
+var plainPerson ={};
 
+function buildPerson(person, nameString, age){
+    person.name=nameString;
+    person.age=age;
+    return person;
+}
+var completePerson = buildPerson(plainPerson, "Michael", "29");
+console.log(completePerson);
+console.log(completePerson.name);
+console.log(completePerson.age);
 
 /*
     # Display values of objects that are inside an array
@@ -61,8 +83,69 @@
             =====
             ...
  */
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"  
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
 
-
+function printProcessedOrders(orders){
+    for(var i=0;i<orders.length;i++){
+        //console.log(orders[i]);
+         console.log("=====");
+         console.log("id: " + orders[i].id);
+         console.log("purchase date: " + orders[i].date);
+         console.log("purchase total: " + orders[i].total);
+    }
+}
+printProcessedOrders(arrayOfObjects);
 /*
     # Addition with an object
         Declare a new variable named sumObj and set it to be a new object with the properties `a`, `b`, and `result`. Properties 'a' and 'b' will be set to a number and result will be 'undefined.'
@@ -73,6 +156,19 @@
 
         Go ahead and create some more objects and pass them to this function. Have fun with it.
 */
+var sumObj = {
+    a: 25,
+    b: 32,
+    result: undefined
+};
+
+function objectAddition(obj){
+    obj.result=obj.a+obj.b;
+    return obj;
+}
+
+var sumObjResult = objectAddition(sumObj);
+console.log(sumObjResult);
 
 
 /*
@@ -89,14 +185,30 @@
         Invoke this function and pass in your object. Further test by changing the values of the object being passed in or **create more** objects and invoke your function multiple times.
  */
 
+function printObj(obj){
+    var output=obj.a+" + "+obj.b+" = "+obj.result;
+    return output;
+}
 
+console.log(printObj(sumObj));
 /*
     # Putting stuff in `plainBox`
         Declare a function named putInPlainBox and a single parameter which will be an object. Within this function, write a FOR loop that adds **10** random number values to the array referenced at the `contents` property of the object being passed in. This function will return the object.
 
         Invoke your function and pass in your object (which should be `plainBox`), store the result to a variable named plainBoxResult and use `console.log` to inspect your results.
  */
-
+ function putInPlainBox(obj){
+    
+    for(var i=0;i<10;i++){
+        var randomNumber = Math.floor((Math.random()*10)+1);
+        //console.log(randomNumber);
+        obj.contents.push(randomNumber);
+    }
+    return obj;
+        //Math.floor((Math.random()*10)+1);
+ }
+putInPlainBox(plainBox);
+console.log(plainBox);
 
 /*
     # Detecting transmission
@@ -106,6 +218,7 @@
 
         Invoke your function and pass in your stockCar object, store the result to a variable named isAutomaticTransmission and use `console.log` to inspect your results.
  */
+ 
 
 
 /*
